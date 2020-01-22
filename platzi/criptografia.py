a52573f470c347bce8a7a075675548af7fc3dfed
pablo-ops <ppmoya08@gmail.com>
2020-01-22 16:07:18 -0300

    error al copiar(ignorar)

--- platzi/criptografia.py
+++ platzi/criptografia.py
@@ -1,36 +1,74 @@
 # -*- coding: utf-8 -*-
 
 KEYS = {
-    'a' : 'q',
-    's' : 'w',
-    'd' : 'e',
-    'f' : 'r',
-    'g' : 't',
-    'h' : 'y',
-    'j' : 'u',
-    'k' : 'i',
-    'l' : 'o',
-    'ñ' : 'p',
-    'q' : 'z',
-    'w' : 'x',
-    'e' : 'c',
-    'r' : 'v',
-    't' : 'b',
-    'y' : 'n',
-    'u' : 'm',
-    'i' : ',',
-    'o' : '.',
-    'p' : '-',
-    'z' : 'A',
-    'x' : 'B',
-    'c' : 'C',
-    'v' : 'D',
-    'b' : 'E',
-    'n' : 'F',
-    'm' : 'G',
-    ',' : 'H',
-    '.' : 'J',
-    '-' : 'K',
+    'a': '!',
+    'b': '¶',
+    'c': '¾',
+    'd': '/',
+    'e': 'H',
+    'f': '¤',
+    'g': '¢',
+    'h': '+',
+    'i': 'R',
+    'j': '±',
+    'k': 'S',
+    'l': '~',
+    'm': '8',
+    'n': '®',
+    'ñ': '1',
+    'o': '½',
+    'p': '4',
+    'q': '"',
+    'r': '5',
+    's': '£',
+    't': '6',
+    'u': '$',
+    'v': '2',
+    'w': '3',
+    'x': '@',
+    'y': '9',
+    'z': '0',
+    'A': '§',
+    'B': 'p',
+    'C': 'l',
+    'D': '¬',
+    'E': 'm',
+    'F': '&',
+    'G': 'g',
+    'H': '×',
+    'I': '-',
+    'J': 'r',
+    'K': 'µ',
+    'L': 'd',
+    'M': '¥',
+    'N': 'v',
+    'Ñ': 'Æ',
+    'O': '¦',
+    'P': 'c',
+    'Q': '#',
+    'R': 'x',
+    'S': '¼',
+    'T': 'z',
+    'U': '?',
+    'V': '÷',
+    'W': '©',
+    'X': 'a',
+    'Y': '%',
+    'Z': 'q',
+    '0': 'co',
+    '1': 'uo',
+    '2': 'ds',
+    '3': 'ts',
+    '4': 'cr',
+    '5': 'cc',
+    '6': 'ss',
+    '7': 'se',
+    '8': 'oo',
+    '9': 'ne',
+    '.': 'po',
+    ',': 'ca',
+    '?': 'pa',
+    '!': 'an',
 }
 
 
@@ -50,8 +88,23 @@ def cifrar(msj):
     return ' '.join(msj_cifrado)
 
 
-def descifrar():
-    pass
+def descifrar(message):
+    words = message.split(' ')
+    decipher_message = []
+
+    for word in words:
+        decipher_word = ''
+
+        for letter in word:
+
+            for key, value in KEYS.items():
+                if value == letter:
+                    decipher_word += key
+        
+        decipher_message.append(decipher_word)
+    
+    return ' '.join(decipher_message)
+
 
 def run():
     while True:
@@ -69,7 +122,9 @@ def run():
             print(msj_cifrado)
 
         if com1 == 'd':
-            descifrar()
+            message = str(input('Escribe tu mensaje cifrado: '))
+            decypher_message = descifrar(message)
+            print(decypher_message)
 
         else:
             print('COMANDO NO ENCONTRADO! ')
