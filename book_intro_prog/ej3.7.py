0aa0a3a85860a503fa0310fae3923ed7c10ce9c3
pablo-ops <ppmoya08@gmail.com>
2020-02-01 16:10:04 -0300

    Ordeno stuff

--- book_intro_prog/ej3.7.py
+++
@@ -1,6 +0,0 @@
-if __name__ == "__main__":
-    num1 = int(input('1°er número a súmar: '))
-    num2 = int(input('\n2°do número a súmar: '))
-
-    print('\nLa suma de ambos números es: {}\n\n'.format(num1 + num2))
-
--- book_intro_prog/ej3.8.py
+++
@@ -1,5 +0,0 @@
-if __name__ == "__main__":
-    print('CONVERSOR METROS --> MILIMETROS')
-    metros = (input('Metros: '))
-
-    print('{} metros son igual a {} milimetros'.format(metros, metros*1000))  
